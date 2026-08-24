e37e84c900658d35b6b12c327f46e579b60f660a
LAjoyan <ajoyan.lilit@gmail.com>
2026-08-24 22:54:18 +0200

    feat: implement button interrupt with debouncing

--- 03_hardware_fundamentals/main.py
+++ 03_hardware_fundamentals/main.py
@@ -9,5 +9,16 @@ green = Pin(14, 1)
 
 button = Pin(13, 0, Pin.PULL_UP)
 
+button_state = {"pressed": False, "last_interrupt_time": 0}
 
 buzzer = Pin(11, 1)
+
+
+def button_callback(pin):
+    current_time = time.ticks_ms()
+    if (current_time - button_state["last_interrupt_time"]) > 200:
+        button_state["pressed"] = False if button_state["pressed"] else True
+        button_state["last_interrupt_time"] = current_time
+
+
+button.irq(trigger=Pin.IRQ_FALLING, handler=button_callback)
