a1a9755313194939dff25dec2ed117123a274146
LAjoyan <ajoyan.lilit@gmail.com>
2026-08-31 23:01:23 +0200

    feat: add weather api request to fetch current temperature

--- 06_wifi/main.py
+++ 06_wifi/main.py
@@ -1,9 +1,19 @@
+import time
 from wifi import connect_wifi
 from machine import Pin
+import requests
 
-status_led = Pin(15,1)
+time.sleep(0.1)
 
-print(connect_wifi())
+status_led = Pin(15,Pin.OUT)
 
 if connect_wifi():
     status_led.value(1)
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=59.3613&longitude=17.9711&current=temperature_2m&timezone=UTC"
+
+response = requests.get(url).json()
+
+outdoor_temperature = response.get("current").get("temperature_2m")
+
+print(f"Outdoor temperature is {outdoor_temperature} °C ")
